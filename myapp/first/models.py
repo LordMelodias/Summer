a9512bff13e889befe7ed3abd55d5a142dbb4a50
Rohit Chawhan <105154639+LordMelodias@users.noreply.github.com>
2024-05-08 07:08:59 +0000

    modified:   myapp/first/models.py 	modified:   myapp/first/models.py

--- myapp/first/models.py
+++ myapp/first/models.py
@@ -1,8 +1,2 @@
 from django.db import models
 
-# Create your models here.
-class register(models.Model):
-    name = models.CharField(max_length=50)
-    phone = models.IntegerField()
-    age = models.IntegerField()
-    password = models.CharField(max_length=20)
