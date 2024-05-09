e82091095f5ae0eea5e1044b875e5862c624d113
Rohit Chawhan <105154639+LordMelodias@users.noreply.github.com>
2024-05-09 13:54:30 +0000

    Project 2: We create form using forms.py 	modified:   form/upload/forms.py

--- form/upload/forms.py
+++ form/upload/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+
+# create form 
 class employee(forms.Form):
     username = forms.CharField(label="Enter Username",max_length=20)
     email = forms.EmailField(label="Enter Email")
