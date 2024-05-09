088420e14860506ff65e2d6b0ad2e97af27f1c58
Rohit Chawhan <105154639+LordMelodias@users.noreply.github.com>
2024-05-09 15:56:06 +0000

    Project 3: We connecting the Django with MongoDB and Mysql 	modified:   README.md 	modified:   databaseconnection/databaseconnection/settings.py

--- databaseconnection/databaseconnection/settings.py
+++ databaseconnection/databaseconnection/settings.py
@@ -74,18 +74,17 @@ WSGI_APPLICATION = 'databaseconnection.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# Connection Code For MySQl (Xaamp)
+# Connection Code For MySQl (Xaamp).
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
 #         'NAME': 'django',
 #         'USER': 'root',
-#         'PASSWORD': '',  # Assuming the root user has no password
+#         'PASSWORD': '', 
 #         'HOST': 'localhost',
 #         'PORT': '3306',
 #     }
 # }
-
 
 # Connection Code For MongoDB
 DATABASES = {
