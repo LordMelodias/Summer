cbec9af4117f9a7b9d18e253763ae6cffc89437f
Rohit Chawhan <105154639+LordMelodias@users.noreply.github.com>
2024-05-18 12:19:02 +0000

    Add Plans page views 	modified:   travel/myapp/views.py

--- travel/myapp/views.py
+++ travel/myapp/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def destination(request):
+    return render(request, 'destination.html')
+
