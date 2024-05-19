1d6419afbebb3026711e75f5161e68c47017326a
Rohit Chawhan <105154639+LordMelodias@users.noreply.github.com>
2024-05-19 11:31:29 +0000

    Add Host table which is admin user table 	modified:   travel/myapp/admin.py

--- travel/myapp/admin.py
+++ travel/myapp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import User, Contact
+from .models import User, Contact, Host
 # Register your models here.
 
 admin.site.register(User)
 
 admin.site.register(Contact)
+
+admin.site.register(Host)
