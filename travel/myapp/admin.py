18aad49619fe39fe3c889efd6b71a12a26fee52a
Rohit Chawhan <105154639+LordMelodias@users.noreply.github.com>
2024-05-27 11:45:13 +0000

    modified:   travel/myapp/admin.py

--- travel/myapp/admin.py
+++ travel/myapp/admin.py
@@ -2,8 +2,3 @@ from django.contrib import admin
 from .models import User, Contact, Host
 # Register your models here.
 
-admin.site.register(User)
-
-admin.site.register(Contact)
-
-admin.site.register(Host)
