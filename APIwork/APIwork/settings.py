ff371c0625e0c1df1b941cc1c1fc9f3a116be566
Rohit Chawhan <105154639+LordMelodias@users.noreply.github.com>
2024-05-31 17:44:29 +0530

    Setup Setting files 	modified:   APIwork/APIwork/settings.py

--- APIwork/APIwork/settings.py
+++ APIwork/APIwork/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -113,7 +112,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
