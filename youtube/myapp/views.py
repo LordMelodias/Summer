8e2040b94b075a7837a39a56d5770aebdeb30db9
Rohit Chawhan <105154639+LordMelodias@users.noreply.github.com>
2024-06-04 10:12:00 +0530

    modified:   youtube/myapp/views.py

--- youtube/myapp/views.py
+++ youtube/myapp/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
 def home(request):
     return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'your_video/about.html')
+
+
+def channel_video(request):
+    return render(request, 'your_video/index.html')
+
+def community(request):
+    return render(request, 'your_video/community.html')
+
+def video(request):
+    return render(request, 'your_video/video.html')
