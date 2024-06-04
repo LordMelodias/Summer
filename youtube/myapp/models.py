1f6464271c7c80fce6a3371c7075c27eacffa81c
Rohit Chawhan <105154639+LordMelodias@users.noreply.github.com>
2024-06-04 10:20:20 +0530

    modified:   youtube/myapp/models.py

--- youtube/myapp/models.py
+++ youtube/myapp/models.py
@@ -1,3 +1,41 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Video(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField(max_length=300)
+    path = models.CharField(max_length=60)
+    datetime = models.DateTimeField(blank=False, null=False) #todo: auto_now=True
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    number_of_views = models.IntegerField(blank=True, default=0)
+
+class Comment(models.Model):
+    text = models.TextField(max_length=300)
+    datetime = models.DateTimeField(auto_now=True, blank=False, null=False)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
+
+class Channel(models.Model):
+    channel_name = models.CharField(max_length=50, blank=False, null=False)
+    subscribers = models.IntegerField(default=0, blank=False, null=False)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+class Like(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
+
+class Dislike(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
+
+class Video_View(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
+    datetime = models.DateTimeField(default=timezone.now)
+
+class Channel_Subscription(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
