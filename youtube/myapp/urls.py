60553e79650ed52a8a550799dfd22e599691bc80
Rohit Chawhan <105154639+LordMelodias@users.noreply.github.com>
2024-06-07 10:04:12 +0530

    modified:   youtube/myapp/urls.py

--- youtube/myapp/urls.py
+++ youtube/myapp/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path("register", views.register, name='register'),
     path("login_view", views.login_view, name='login_view'),
     path("save_register", views.save_register, name='save_register'),
-    
+     path('otp/<str:email>/', views.otp, name='otp'),
 ]
