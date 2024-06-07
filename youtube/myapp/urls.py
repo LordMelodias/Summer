331892e7a64fe0844671d8702a43f87f8890d6a5
Rohit Chawhan <105154639+LordMelodias@users.noreply.github.com>
2024-06-07 10:03:25 +0530

    modified:   youtube/myapp/urls.py

--- youtube/myapp/urls.py
+++ youtube/myapp/urls.py
@@ -3,11 +3,11 @@ from myapp import views
 
 urlpatterns = [
     path("", views.home, name='home'),
-    path("/channel_video", views.channel_video, name='channel_video'),
-    path("/about", views.about, name='about'),
-    path("/community", views.community, name='community'),
-    path("/video", views.video, name='video'),
-    path("/login", views.login, name="login"),
-    path("/register", views.register, name='register'),
-     path("login_view", views.login_view, name='login_view'),
+    path("channel_video", views.channel_video, name='channel_video'),
+    path("about", views.about, name='about'),
+    path("community", views.community, name='community'),
+    path("video", views.video, name='video'),
+    path("login", views.login, name="login"),
+    path("register", views.register, name='register'),
+    path("login_view", views.login_view, name='login_view'),
 ]
